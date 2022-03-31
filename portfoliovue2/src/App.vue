5a8e2cd9e36fd9abad0bce1aa3e55f60e6d48be4
Adm's <lemopidr1@gmail.com>
2022-03-31 13:14:35 +0200

    Created Header

--- portfoliovue2/src/App.vue
+++ portfoliovue2/src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
-    <div class="header"></div>
+    <Header />
     <div class="main">
       <router-view />
     </div>
-    <div class="footer"></div>
   </div>
 </template>
 
 <script>
+import Header from "@/components/header.vue";
+
 export default {
   name: "App",
+  components: {
+    Header,
+  },
 };
 </script>
 
 
 <style lang="scss">
 @import "./assets/styles/tailwind.scss";
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Train+One&family=Ubuntu&family=Zen+Tokyo+Zoo&display=swap');
+
 </style>
---
+++ portfoliovue2/src/components/header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="logoWrapper">
+      <router-link to="/" class="logo" :active-class="'active'"
+        >adm’s</router-link
+      >
+    </div>
+    <div class="links">
+      <router-link to="/contact" class="link" :active-class="'active'"
+        >about me</router-link
+      >
+      <router-link to="/contact" class="link" :active-class="'active'"
+        >projects</router-link
+      >
+      <router-link to="/contact" class="link" :active-class="'active'"
+        >contact</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: url("../assets/img/header/background.png");
+  background-size: 120rem;
+  background-position: 70%, center;
+  @apply flex justify-around bg-no-repeat overflow-hidden h-72;
+  & .logoWrapper {
+    & .logo {
+      @apply text-5xl relative -left-24 top-2 text-white;
+      font-family: "Train One";
+    }
+  }
+  & .links {
+    @apply flex relative -left-24;
+    & .link {
+      @apply mx-14 text-5xl text-white mt-4 h-10;
+      font-family: "Zen Tokyo Zoo";
+    }
+  }
+}
+</style>
